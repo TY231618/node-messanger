51819e6d72de3123e2665ab9b5b16a4aa608dadf
Tony <anthony.c.young18@gmail.com>
2018-01-03 10:48:40 +0000

    created basic get endpoint to display message array

--- server.js
+++ server.js
@@ -3,6 +3,21 @@ let app = express();
 
 app.use(express.static(__dirname));
 
+let messages = [
+  {
+    name: 'Tony',
+    message: 'HELLO'
+  },
+  {
+    name: 'Sally',
+    message: 'Hi'
+  }
+];
+
+app.get('/messages', (req, res) => {
+  res.send(messages);
+});
+
 let server = app.listen(3000, () => {
   console.log('server is listening on port ', server.address().port);
 });
