285792433b0a8c4bf6a602759af8df45a0e54aae
Tony <anthony.c.young18@gmail.com>
2018-01-03 12:42:55 +0000

    added a badword filter to postMessage to demonstrate callback hell

--- server.js
+++ server.js
@@ -27,6 +27,15 @@ app.post('/messages', (req, res) => {
   message.save((err) => {
     if(err) sendStatus(500);
 
+    Message.findOne({message: 'badword'}, (err, censored) => {
+      if(censored) {
+        console.log('censored word found: ', censored);
+        Message.remove({_id: censored.id}, (err) => {
+          console.log('removed censored message');
+        });
+      }
+    });
+
     io.emit('message', req.body);
     res.sendStatus(200);
   });
