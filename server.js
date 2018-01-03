b66a96e6a1c8c5e26ef55282eb79c091c563463b
Tony <anthony.c.young18@gmail.com>
2018-01-03 10:26:15 +0000

    added callback to app.listen

--- server.js
+++ server.js
@@ -3,4 +3,6 @@ let app = express();
 
 app.use(express.static(__dirname));
 
-app.listen(3000);
+let server = app.listen(3000, () => {
+  console.log('server is listening on port ', server.address().port);
+});
