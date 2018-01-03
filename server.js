89fee9c809f7ea2b83c0c02595d80070a25498eb
Tony <anthony.c.young18@gmail.com>
2018-01-03 13:16:25 +0000

    trying to use async

--- server.js
+++ server.js
@@ -23,6 +23,18 @@ app.get('/messages', (req, res) => {
   });
 });
 
+//original post request
+// app.post('/messages', (req, res) => {
+//   let message = new Message(req.body);
+
+//   message.save((err) => {
+//     if(err) sendStatus(500);
+
+//     io.emit('message', req.body);
+//     res.sendStatus(200);
+//   });
+// });
+
 //callback hell post request and remove happens after the save!!!
 // app.post('/messages', (req, res) => {
 //   let message = new Message(req.body);
@@ -45,40 +57,48 @@ app.get('/messages', (req, res) => {
 // });
 
 //using promises
-app.post('/messages', (req, res) => {
-  let message = new Message(req.body);
-
-  message.save()
-    .then(() => {
-      console.log('saved');
-      return Message.findOne({message: 'badword'});
-    })
-    .then((censored) => {
-      if(censored) {
-        console.log('censored word found: ', censored);
-        return Message.remove({_id: censored.id});
-      }
-
-      io.emit('message', req.body);
-      res.sendStatus(200);
-    })
-    .catch((err) => {
-      res.sendStatus(500);
-      return console.error(err);
-    });
-});
-
-//original post request
 // app.post('/messages', (req, res) => {
 //   let message = new Message(req.body);
 
-//   message.save((err) => {
-//     if(err) sendStatus(500);
+//   message.save()
+//     .then(() => {
+//       console.log('saved');
+//       return Message.findOne({message: 'badword'});
+//     })
+//     .then((censored) => {
+//       if(censored) {
+//         console.log('censored word found: ', censored);
+//         return Message.remove({_id: censored.id});
+//       }
 
-//     io.emit('message', req.body);
-//     res.sendStatus(200);
-//   });
+//       io.emit('message', req.body);
+//       res.sendStatus(200);
+//     })
+//     .catch((err) => {
+//       res.sendStatus(500);
+//       return console.error(err);
+//     });
 // });
+
+//now using async
+app.post('/messages', async (req, res) => {
+
+  let message = new Message(req.body);
+
+  let savedMessage = await message.save();
+
+  console.log('saved');
+  
+  let censored = await Message.findOne({message: 'badword'});
+
+  if(censored) {
+    await Message.remove({_id: censored.id});
+  } else {
+    io.emit('message', req.body);
+  }
+
+  res.sendStatus(200);
+});
 
 io.on('connection', (socket) => {
   console.log('a user is connected');
