25487b7e5dd041f3dcc1b98c0d76bd23e604fd7b
Tony <anthony.c.young18@gmail.com>
2018-01-03 11:43:16 +0000

    added mongoose and set up basic db connection

--- server.js
+++ server.js
@@ -1,13 +1,15 @@
 let express = require('express');
 let bodyParser = require('body-parser');
 let app = express();
-
+let mongoose = require('mongoose');
 let http = require('http').Server(app);
 let io = require('socket.io')(http);
 
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
+
+let dbURL = 'mongodb://test:test@ds113925.mlab.com:13925/node-messanger';
 
 let messages = [
   {
@@ -32,6 +34,10 @@ app.post('/messages', (req, res) => {
 
 io.on('connection', (socket) => {
   console.log('a user is connected');
+});
+
+mongoose.connect(dbURL, (err) => {
+  console.log('mongoose connection err: ', err);
 });
 
 let server = http.listen(3000, () => {
